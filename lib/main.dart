9ba4426463553e455af035f5d64e0b086483eadf
Shadow_Plays <shadowplayschannel@gmail.com>
2025-04-16 08:21:29 +0000

    16-4-2025 10:21

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import '../theme/theme_provider.dart';
+import '../theme/app_themes.dart';
 import '../pages/main_page.dart';
 
 void main() async {
@@ -8,14 +11,24 @@ void main() async {
   // ignore: unused_local_variable
   var box = await Hive.openBox('mybox');
 
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(lightTheme),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MainPage(), debugShowCheckedModeBanner: false);
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return MaterialApp(
+        theme: themeProvider.themeData,
+        home: MainPage(),
+        debugShowCheckedModeBanner: false);
   }
 }
